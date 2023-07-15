7416f0a19979aff0753f2bb01a31880f64410819
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-07-15 18:01:29 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_13_152002) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_14_190740) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "photo"
+    t.decimal "price"
+    t.text "model"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cars_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -21,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_13_152002) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cars", "users"
 end
