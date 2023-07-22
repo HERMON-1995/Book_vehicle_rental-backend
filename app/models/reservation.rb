7f35e46ec4b09a47c819d75b52ea76423b6c19c4
theophilus doku <theodoku89@gmail.com>
2023-07-22 14:52:34 +0000

    Modify reservation.rb

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,23 +1,11 @@
 class Reservation < ApplicationRecord
-  validates :reservation_date, presence: true 
-  validates :returned_date, presence: true 
-  validates :car_id, presence: true
+  belongs_to :car
+  belongs_to :user
+
+  validates :user_id, presence: true
+  validates :reservation_date, presence: true
+  validates :returned_date, presence: true
   validates :user_id, presence: true
   validates :city, presence: true
-
-  # associations 
-  belongs_to :user 
-  belongs_to :car
 end
-class Reservation < ApplicationRecord
-    belongs_to :car
-    belongs_to :user
-
-    validates :user_id, presence: true
-    validates :reservation_date, presence: true
-    validates :returned_date, presence: true
-    validates :user_id, presence: true
-    validates :city, presence: true
-end
-
   
