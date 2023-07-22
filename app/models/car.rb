7e2dc9ef7bbf5009019a538c1ab71f69724286ae
theophilus doku <theodoku89@gmail.com>
2023-07-22 10:17:19 +0000

    Modify car.rb file

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,9 @@
 class Car < ApplicationRecord
   belongs_to :user
 
+  has_many :reservations, dependent: :destroy
+  #has_many :user, through: :reservations
+
   validates :name, presence: true
   validates :model, presence: true
   validates :price, presence: true
