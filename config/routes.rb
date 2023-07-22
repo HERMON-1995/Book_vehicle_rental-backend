0870d309ec8bfd7113b805f407e76da323245ad8
theophilus doku <theodoku89@gmail.com>
2023-07-22 10:19:24 +0000

    Modify routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: %i[index create destroy]
       resources :cars, only: %i[index create show update destroy]
+      resources :reservations
       post 'login', to: 'authentication#create'
       post 'register', to: 'users#create'
+      get '/api/v1/reservations', to: 'api/v1/reservations#index'
     end
   end
 end
