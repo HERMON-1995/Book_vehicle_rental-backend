0dd8788983d5b7b7f9aa5440e1e2a77ff7a25390
theophilus doku <theodoku89@gmail.com>
2023-07-22 14:52:48 +0000

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
       resources :reservations 
       post 'login', to: 'authentication#create'
       post 'register', to: 'users#create'
-      get '/api/v1/reservations', to: 'api/v1/reservations#index'
     end
   end
 end
