f393a1fbe27f72d37e543c89a0399182ed8cc53c
Gmanlove <emmagmanc@gmail.com>
2023-07-24 20:41:07 +0100

    Add test for reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,4 @@
+# app/models/reservation.rb
 class Reservation < ApplicationRecord
   belongs_to :car
   belongs_to :user
@@ -5,6 +6,23 @@ class Reservation < ApplicationRecord
   validates :user_id, presence: true
   validates :reservation_date, presence: true
   validates :returned_date, presence: true
-  validates :user_id, presence: true
   validates :city, presence: true
+
+  # Define the scopes
+  scope :recent, -> { order(created_at: :desc) }
+  scope :by_city, -> (city) { where(city: city) }
+
+  def total_cost
+    # Calculate the duration of the reservation in days
+    duration_in_days = (returned_date - reservation_date).to_i
+
+    # Get the price of the car
+    car_price = car.price
+
+    # Calculate the total cost based on the duration and car price
+    total_cost = duration_in_days * car_price.to_i
+
+    # Return the total cost as a float with 2 decimal places
+    total_cost.to_f / 100
+  end
 end
--- test/factories/reservations.rb
+++ test/factories/reservations.rb
@@ -1,7 +1,10 @@
+# spec/factories/reservation.rb
 FactoryBot.define do
   factory :reservation do
     city { 'MyString' }
     reservation_date { '2023-07-20' }
-    return_date { '2023-07-20' }
+    returned_date { '2023-07-21' }
+    association :car # Ensure valid association with Car
+    association :user # Ensure valid association with User
   end
 end
