f10e9c8fa55dbea2e944ee51452919b17dd65305
Gmanlove <emmagmanc@gmail.com>
2023-07-26 00:54:54 +0100

    Update file

---
+++ spec/integration/reservations_spec.rb
@@ -0,0 +1,28 @@
+require 'swagger_helper'
+
+describe 'Reservations API', swagger_doc: 'v1/swagger.json' do
+  path '/api/v1/reservations' do
+    post 'Creates a reservation' do
+      tags 'Reservations'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :reservation, in: :body, schema: {
+        type: :object,
+        properties: {
+          city: { type: :string },
+          date: { type: :string },
+          time: { type: :string },
+          duration: { type: :integer }
+        },
+        required: ['city', 'date', 'time', 'duration']
+      }
+
+      response '400', 'Bad Request' do
+        let(:reservation) { { city: '', date: '2023-07-23', time: '09:00', duration: 2 } }
+        run_test! do |response|
+          expect(response.body).to match("{\"error\":\"Something went wrong\"}")
+        end
+      end
+    end
+  end
+end
