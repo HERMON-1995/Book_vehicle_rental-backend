02ec7c13d9e404491b03a645d116e64bf05f1f15
Gmanlove <emmagmanc@gmail.com>
2023-07-26 00:55:48 +0100

    Add api doc

---
+++ spec/integration/authentication_spec.rb
@@ -0,0 +1,43 @@
+require 'swagger_helper'
+require 'factory_bot_rails'
+require 'rswag/specs'
+
+describe 'Authentication API', swagger_doc: 'v1/swagger.json' do
+  include FactoryBot::Syntax::Methods
+
+  before do
+    # Create a user with the correct username and password using FactoryBot
+    create(:user, username: 'emma', password: 'password123')
+  end
+
+  path '/api/v1/authentication' do
+    post 'Authenticates a user' do
+      tags 'Authentication'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          username: { type: :string },
+          password: { type: :string }
+        },
+        required: ['username', 'password']
+      }
+
+      response '401', 'Unauthorized' do
+        let(:user) { { username: 'emma', password: 'wrongpassword' } }
+        run_test! do |response|
+          expect(response.body).to match(/No such user; check the submitted username/)
+        end
+      end
+
+      response '201', 'Created' do
+        let(:user) { { username: 'emma', password: 'password123' } }
+        run_test! do |response|
+          data = JSON.parse(response.body)
+          expect(data['username']).to eq('emma') # Ensure that the 'username' field is correct
+        end
+      end
+    end
+  end
+end
